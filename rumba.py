fbf035c2e50b8a2fcc22ff58e6a9ced7adaac9e7
dmunofer <diego.fmz@hotmail.com>
2023-02-16 18:56:53 +0100

    cambios

--- rumba.py
+++ rumba.py
@@ -6,3 +6,10 @@ def MenuPrincipal():
     tittle_text = Label(root, text="Roomba")
     tittle_text.config(font=18)
     tittle_text.pack(pady=2)
+    main_text = Label(root, text="¿Cuantas zonas hay que limpiar?")
+    main_text.pack(pady=80)
+    main_text.config(font=16)
+
+    s = Spinbox(root, from_=0, to=100000, increment=1)
+    s.config()
+    s.pack(pady=10)
