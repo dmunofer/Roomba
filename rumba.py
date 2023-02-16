53fbbc9be2523c40dfa41401ac1988b73e1ecf02
dmunofer <diego.fmz@hotmail.com>
2023-02-16 18:55:54 +0100

    cambios

--- rumba.py
+++ rumba.py
@@ -1,3 +1,8 @@
 from tkinter import *
 root = Tk()
 root.geometry("650x650")
+
+def MenuPrincipal():
+    tittle_text = Label(root, text="Roomba")
+    tittle_text.config(font=18)
+    tittle_text.pack(pady=2)
